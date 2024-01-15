1cd6feea1736921d2789ba86a07a4de66be2f59b
UtsavKumar126/ <guptautsavkumar126@gmail.com>
2024-01-15 20:55:33 +0530

    third

--- src/components/App.js
+++ src/components/App.js
@@ -24,7 +24,7 @@ const App = () => {
           </thead>
           <tbody>
             {
-              !data?<h4>No data found</h4>:
+              !data?<h4>No data found to display.</h4>:
               data.map((ele)=>
               <tr>
                 <td>{ele.first_name}</td>
